6ee90e23fa85392939e7c1355b68cfe79c6ffb18
Ryan Bard <john.ryan.bard@gmail.com>
2017-09-26 10:46:58 -0400

    Made code follow pep8

--- scrape_to_json.py
+++ scrape_to_json.py
@@ -1,4 +1,6 @@
-import requests, bs4, json
+import requests
+import bs4
+import json
 
 # sudo apt-get install python-bs4
 # sudo apt-get install python-requests
@@ -18,21 +20,29 @@ def extract_time_slot(i, time_slot):
     session_elements = time_slot.select('div.schedule-item')
     sessions = map(extract_session, session_elements)
     return {
-        'name': 'Slot {index}'.format(index = i + 1),
+        'name': 'Slot {index}'.format(index=i + 1),
         'sessions': sessions
     }
-    
+
 
 def main():
-    res = requests.api.request('get', 'https://atlantacodecamp.com/2017/Schedule', verify=False)
+    res = requests.api.request(
+            'get',
+            'https://atlantacodecamp.com/2017/Schedule',
+            verify=False
+    )
     res.raise_for_status()
     soup = bs4.BeautifulSoup(res.text, 'lxml')
     time_slot_elements = soup.select('div.col-lg-11 > div.row')
-    time_slots = map(lambda (i, time_slot): extract_time_slot(i, time_slot), enumerate(time_slot_elements))
+    time_slots = map(
+            lambda (i, time_slot): extract_time_slot(i, time_slot),
+            enumerate(time_slot_elements)
+    )
     result = {
         'slots': time_slots
     }
-    print(json.dumps(result, sort_keys = True, indent = 4))
+    print(json.dumps(result, sort_keys=True, indent=4))
+
 
 if __name__ == '__main__':
     main()
